1b67395365de44b113a1caff34af68624d8e539a
wagaaa <yui12349856@live.com>
2024-04-09 03:47:14 +0000

    updated

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,9 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
-// Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
@@ -30,4 +27,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        2 * num
+    }
 }
